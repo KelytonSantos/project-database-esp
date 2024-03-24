f18d752ed73cc47c0f8463e7b7cdf064c3176cbe
Kelyton de Lucas <kelytonlucas@gmail.com>
2024-03-24 20:09:03 -0300

    adicionado .ino de requisição post

--- SendData/EspCode/EspCode.ino
+++ SendData/EspCode/EspCode.ino
@@ -1,8 +1,51 @@
-void setup(){
+#include <HTTPClient.h>
+#include <WiFi.h>
 
+const char *ssid = "MultilaserPRO_ZTE_2.4G_TDEDEd";
+const char *password = "****";
+
+void setup() {
+  	Serial.begin(115200);
+  	while (!Serial) {
+    		;
+  	}
+
+  	delay(100);
+
+  	WiFi.begin(ssid, password);
+
+  	while (WiFi.status() != WL_CONNECTED) {
+    	delay(1000);
+    	Serial.println("Conectando ao WiFi...");
+  	}
+
+  	Serial.println("Conectado ao WiFi!");
 }
 
-void loop(){
+void loop() {
+  	if (Serial.available()) {
+		HTTPClient http;
+    		
+		String data = Serial.readStringUntil('\n'); // Ler a entrada serial até a quebra de linha (\n)
+    		
+		Serial.print("Enviando dado para o servidor: ");
+    		Serial.println(data);
 
+    		String serverAddress = "http://192.168.1.9:8080/api/data";
+
+    		http.begin(serverAddress);
+    		http.addHeader("Content-Type", "application/json");
+    		
+		String payload = "{\"temp\": " + data + "}";
+		int httpResponseCode = http.POST(payload);
+    		
+		String response = http.getString();
+
+    		Serial.print("Resposta do servidor: ");
+    		Serial.println(response);
+
+		http.end();
+  	}
 }
+
 
