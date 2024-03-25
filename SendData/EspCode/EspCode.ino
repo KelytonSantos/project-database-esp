3bebe42143dea025fd7bb6da4155b5f807946baf
Kelyton de Lucas <kelytonlucas@gmail.com>
2024-03-24 21:20:31 -0300

    Adicionado sensor DHT11(persistency working)

--- SendData/EspCode/EspCode.ino
+++ SendData/EspCode/EspCode.ino
@@ -1,13 +1,24 @@
+//---OnLooker
+
 #include <HTTPClient.h>
 #include <WiFi.h>
+#include <DHT.h>
+
+#define DHTPIN 23     // Pino ao qual o sensor está conectado
+#define DHTTYPE DHT11 // Tipo do sensor
 
 const char *ssid = "MultilaserPRO_ZTE_2.4G_TDEDEd";
 const char *password = "****";
 
+DHT dht(DHTPIN, DHTTYPE);
+
+float HumiditySensor();
+float TemperatureSensor();
+
 void setup() {
   	Serial.begin(115200);
   	while (!Serial) {
-    		;
+    		; //fica preso no loop vazio enquanto nao estiver pronto a comunic serial
   	}
 
   	delay(100);
@@ -20,16 +31,17 @@ void setup() {
   	}
 
   	Serial.println("Conectado ao WiFi!");
+	dht.begin();
 }
 
 void loop() {
-  	if (Serial.available()) {
+	float temperatura = TemperatureSensor();
+	float umidade = HumiditySensor();
+
+  	if (temperatura != -1 && umidade != -1) {
 		HTTPClient http;
     		
-		String data = Serial.readStringUntil('\n'); // Ler a entrada serial até a quebra de linha (\n)
-    		
-		Serial.print("Enviando dado para o servidor: ");
-    		Serial.println(data);
+		String data = String(temperatura, 2);
 
     		String serverAddress = "http://192.168.1.9:8080/api/data";
 
@@ -46,6 +58,39 @@ void loop() {
 
 		http.end();
   	}
+  	delay(1000);
+}
+
+
+float HumiditySensor(){
+	delay(1000);
+	float umidade = dht.readHumidity();         // Lê a umidade relativa
+
+  	if (isnan(umidade)) {
+    		Serial.println("Falha ao ler o sensor DHT11!");
+    		return -1;
+  	}
+
+  	//Serial.print(" °C, Umidade: ");
+  	//Serial.print(umidade);
+  	//Serial.println("%");
+	
+	return umidade;
+}
+
+float TemperatureSensor(){
+	delay(1000); 
+	float temperatura = dht.readTemperature(); 
+	
+	if(isnan(temperatura)){
+		Serial.println("Falha ao ler o sensor DHT11!");
+		return -1;
+	}
+
+	//Serial.print("Temperatura: ");
+  	//Serial.print(temperatura);
+
+	return temperatura;
 }
 
 
